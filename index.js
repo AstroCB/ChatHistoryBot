3ad98ab8b911459be18fd2e6650dd23ee7b15b83
Cameron Bernhardt <cambernhardt@me.com>
2016-10-31 19:54:17 -0400

    Fix author bug

--- index.js
+++ index.js
@@ -138,6 +138,7 @@ app.post('/webhook', function(req, res) {
         var event = events[i];
         if (event.message && event.message.text && messages.length > 0) {
             const msgText = handleMessage(event.message);
+            console.log(msgText);
             sendMessage(event.sender.id, {
                 text: msgText
             });
