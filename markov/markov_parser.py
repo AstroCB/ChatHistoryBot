bf028b7a02651d57eae528430400815acbdf895a
Cameron Bernhardt <cambernhardt@me.com>
2016-11-12 00:38:19 -0500

    Add more Markov sentences

--- markov/markov_parser.py
+++ markov/markov_parser.py
@@ -1,12 +1,15 @@
 import markovify
 
-authors = ["yiyi.txt", "larry.txt", "cam.txt", "jonah.txt", "ghoul.txt"]
+authors = ["yiyi.txt", "larry.txt", "cam.txt"]#, "jonah.txt", "ghoul.txt"]
 
 for a in authors:
     with open(a) as f:
         model = markovify.Text(f.read())
         with open("../markov_sentences/" + a, "w") as d:
             mark = model.make_short_sentence(320) # Messenger length limit
-            while mark is not None:
+            counter = 0 # Limit # of messages to prevent infinite loops
+            while mark is not None and counter < 50000:
                 d.write(mark + "\n")
-                mark = model.make_sentence()
+                mark = model.make_short_sentence(320)
+                print(mark)
+                counter += 1
